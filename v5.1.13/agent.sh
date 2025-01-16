41ac7bcaada9aeaaa527856b87cb7547167f301e
pl <pl@gmail.com>
2025-01-16 16:56:04 +0800

    fix

--- v5.1.13/agent.sh
+++ v5.1.13/agent.sh
@@ -236,7 +236,7 @@ fi
 
 cd /opt
 
-download "https://github.com/LoveesYe/cdnflydadao/raw/main/cdnfly/v5.1.13/agent/$tar_gz_name" "https://github.com/LoveesYe/cdnflydadao/raw/main/cdnfly/v5.1.13/agent/$tar_gz_name" "$tar_gz_name"
+download "https://raw.githubusercontent.com/vasma1984/cdnfly-pin-duo-duo/refs/heads/main/v5.1.13/$tar_gz_name" "https://raw.githubusercontent.com/vasma1984/cdnfly-pin-duo-duo/refs/heads/main/v5.1.13/$tar_gz_name" "$tar_gz_name"
 
 rm -rf $dir_name
 tar xf $tar_gz_name
